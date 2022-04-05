bcecc7b4f893bdd8c4af933c6066917bf598ab33
seojenny <99632954+seojenny@users.noreply.github.com>
2022-04-05 20:34:30 +0800

    Update movie.py

--- movie.py
+++ movie.py
@@ -2,17 +2,18 @@ checkValue=True
 userInfo=[]
 while True:
 
-    name=input("put name :")
+    name=input("what is your name :")
     userInfo.append(name)
-    number=int(input("put number :"))
+    number=int(input("what is your phone number :"))
     userInfo.append(number)
-    movieName=input("put moive name : ")
+    movieName=input("What moive you want to watch : ")
     userInfo.append(movieName)
-    movieTime=input("What time you wanna watch movie?(morning, lunch, afternoon)")
+    movieTime=input("What time you wanna watch movie?(morning, lunch, afternoon):")
     userInfo.append(movieTime)
-    userSeat=input("Which seat you want to sit? (1,2,3,4,5)")
+    userSeat=input("Which seat you want to sit? (1,2,3,4,5,6,7,8,9,10):")
     userInfo.append(userSeat)
-    checkPrint=input("You want to see your INFO? Y/N")
-    if checkPrint=="Y":
-        print(userInfo)
-        break
+    checkPrint=input("You want to see your INFO? Yes/No:")
+    if checkPrint=="Yes":
+          print(userInfo)
+    if checkPrint=="No":
+ 
