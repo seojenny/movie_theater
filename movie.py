2a8af53ac7cb233b29c8e6c04be5a870d6bbf4e2
seojenny <99632954+seojenny@users.noreply.github.com>
2022-04-05 20:18:20 +0800

    Update movie.py

--- movie.py
+++ movie.py
@@ -9,7 +9,7 @@ while True:
     movieName=input("put moive name : ")
     userInfo.append(movieName)
     movieTime=input("What time you wanna watch movie?(morning, lunch, afternoon)")
-    userInfo.append(movieName)
+    userInfo.append(movieTime)
     userSeat=input("Which seat you want to sit? (1,2,3,4,5)")
     userInfo.append(userSeat)
     checkPrint=input("You want to see your INFO? Y/N")
