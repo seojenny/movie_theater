01aeea7c614777dbb6ce37d35580054b470ec132
seojenny <99632954+seojenny@users.noreply.github.com>
2022-04-07 10:32:00 +0800

    Update movie.py

--- movie.py
+++ movie.py
@@ -31,6 +31,43 @@ while rebook=='yes':
     userSeat=input("Which seat you want to sit? (1,2,3,4,5,6,7,8,9,10):")
     userInfo.append(userSeat)"""
     
+#seats
+movie_seats = {{[], [], []}, {[], [], []}, {[], [], []}
+#for morning It
+def movie_seat(chosen_movie, chosen_time):
+    booked_seats=[]
+    while True:
+          movie_index = movies.index(chosen_movie)
+          time_index = times.index(chosen_time)
+          #print seats and check if seat is already booked
+          seats_tct = ''
+          for seat_n in range(1,11):
+               if seat_n in movie_seats[movie_index][time_index]:
+                    seats_txt = seats_txt + 'x'
+               else:
+                    seats_txt = seats_txt + f' {seat_n}'
+               
+          print (seats_txt)
+          #asks for seat and appends it into booked_seats
+          chosen_seat = None
+          while_chosen_seat not in range(1,11):
+               #validate that while not chosen_seat.isnumber():
+               chosen_seat = int(input("what seat do you want?"))
+               
+               if chosen_seat in movie_seats[movie_index][time_index]:
+                    print("seat already has been chosen, please choose a different seat")
+               else:
+                    movie_seats[movie_index][time_index].append(chosen_seat)
+                    booked_seats.append(chosen_seat)
+               
+             #if the user wants it returns the program
+               choose_again = None
+               choose_again = input("do you want to book a seat again?(yes/no): ")
+               if choose_Again == 'n':
+                    print("thank you")
+                    break
+           return booked seats
+    
     print(f"name: {name}, number: {number}, movie: {movie}, time: {movieTime}")
     
     answer = None
